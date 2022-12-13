dad4e468d3aa388fa4f0314811978d9c956c7d09
Batyanoskow <Lyubchenko2910@gmail.com>
2022-12-13 08:52:19 +0200

    Vizualizer

--- dist/js/app.js
+++ dist/js/app.js
@@ -4661,10 +4661,48 @@
                 audio.crossOrigin = "anonymous";
                 audio.volume = 1;
                 let buttonPlay = document.querySelector(".music-header__button");
+                let context = new (window.AudioContext || window.webkitAudioContext);
+                var src = context.createMediaElementSource(audio);
+                var analyser = context.createAnalyser();
+                let canvas = document.getElementById("myCanvas");
+                var ctx = canvas.getContext("2d");
+                src.connect(analyser);
+                analyser.connect(context.destination);
+                analyser.fftSize = 512;
+                ctx.lineWidth = 5;
+                var bufferLength = analyser.frequencyBinCount;
+                var dataArray = new Uint8Array(bufferLength);
+                var WIDTH = canvas.width;
+                var HEIGHT = canvas.height;
+                var barWidth = WIDTH / bufferLength;
+                var barHeight = HEIGHT;
+                console.log(dataArray);
+                function renderFrame() {
+                    analyser.getByteFrequencyData(dataArray);
+                    var x = 0;
+                    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+                    for (var i = 0; i < bufferLength; i++) {
+                        barHeight = dataArray[i] / 10;
+                        ctx.fillStyle = "white";
+                        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+                        x += barWidth + 3;
+                        if (75 === i) {
+                            ctx.fillStyle = "rgba(0,0,0,0)";
+                            ctx.fillRect(x, HEIGHT - barHeight, 10 * barWidth, barHeight);
+                            ctx.font = "20px Montserrat";
+                            ctx.fillStyle = "white";
+                            ctx.fillText("88.9", 425, 28);
+                            x += barWidth + 80;
+                        }
+                    }
+                    requestAnimationFrame(renderFrame);
+                }
+                renderFrame();
                 buttonPlay.addEventListener("click", (function(e) {
                     buttonPlay.classList.toggle("_active");
                     let buttonPlayImg = document.querySelector(".music-header__button picture  source");
                     if (buttonPlay.classList.contains("_active")) {
+                        context.resume();
                         audio.play();
                         buttonPlayImg.setAttribute("srcset", "img/icon-pause.webp");
                     } else {
